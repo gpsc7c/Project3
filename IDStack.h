076099dea4001df475cd948d503e771ef5259e66
gpsc7c <gpsc7c@delmar.umsl.edu>
2024-04-02 06:07:38 -0500

    set up stack structs

---
+++ IDStack.h
@@ -0,0 +1,15 @@
+#ifndef VARSTACK_H
+#define VARSTACK_H
+#include "token.h"
+
+typedef struct eElement{
+	tToken tk;
+	struct element *next;
+}eElement;
+typedef struct sStack{
+	int count;
+	eElement* top;
+}sStack;
+void push(tToken);
+void pop();
+void search(tToken, int);
