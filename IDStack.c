330d414413c44d43246751468c07ed62fda05e91
gpsc7c <gpsc7c@delmar.umsl.edu>
2024-04-04 23:28:21 -0500

    changes to situation

--- IDStack.c
+++ IDStack.c
@@ -2,8 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 #include "IDStack.h"
+#include "scanner.h"
 
-static int MAXSTACK = 128;
+static int MAXSTACK = 127;
 static sStack stack;
-stack.size = 0;
-stack.top = NULL;
+void stackSet(){
+	stack.size = 0;
+	stack.top = NULL;
+}
+void push(tToken tk){
+	int i;
+	if(stack.size > MAXSTACK){
+		fprintf(stderr, "\nERROR: IDStack.c: push Stack overflow."
+				"Stack has %d elements.\n", stack.size);
+		exit()
+	}
+	eElement *element = (eElement*)malloc(sizeof(eElement));
+	element->tk = tokenSetter(tk.ID, tk.tokenInstance, tk.row, tk.column);
+	element->next = stack.top;
+	stack.top = element;
+	stack.size++;	
+}
+void pop(){
+	if(stack.size < 1){
+		fprintf(stderr, "\nERROR: IDStack.c: pop: Stack empty.\n"
+		exit()
+	}
+	eElement* tempholder = stack.top;
+	stack.top = tempholder->next;
+	free(tempHolder)
+	stack.size--;
+}
+void search(tToken tk, int){
+	int pos;	//position in stack
+	eElement* element = stack.top
+	
+	for(pos = 0; pos < stack.size; pos++){
+		if(strcmp(tk.tokenInstance, element->tk.instance == 0){}
+		element = element->next;
+	}
+	
+}
